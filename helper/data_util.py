52b834594b3da799beedc23fe331b6798af6bc27
barnrang <barnrang@gmail.com>
2017-12-03 00:31:18 +0900

    add test

--- helper/data_util.py
+++ helper/data_util.py
@@ -1,5 +1,6 @@
 import random
 import os
+import numpy as np
 class textData(object):
     """docstring for textData.
     data: T *
@@ -12,24 +13,64 @@ class textData(object):
         self.categories = 0
         self.allchar = allchar
         self.nchar = len(allchar)
+    def _randomText(self):
+        return random.randint(0,self.categories-1)
+    def _textNormalize(self,text):
+        replace = {'“':'"','”':'"','’':'\'','‘':'\''}
+        out = text[:]
+        for key in replace:
+            out = out.replace(key,replace[key])
+        return out
     def load_data(self, path='resource'):
+        self.data = []
         data_list = os.listdir(path)
         for item in data_list:
             try:
-                self.data.append(''.join(_ for _ in open(os.path.join(path,item))))
-            except:
+                self.data.append(''.join(self._textNormalize(_) for _ in open(os.path.join(path,item))))
+            except IOError:
                 print("cant load data {}".format(item))
         self.categories = len(self.data)
-    def _randomText():
-        return random.randint(0,self.categories-1)
-    def _textToNum(text):
-        return [self.nchar.find(x) for x in self.allchar]
-    def randomInandOut(l):
+    def textToNum(self,text):
+        return [self.allchar.find(x) for x in text]
+    def numToText(self,num_list):
+        return ''.join(self.allchar[x] for x in num_list)
+    def randomInandOut(self, l_in,l_out, offset=None, batch_num=20):
+        if offset is None:
+            offset = l_in - l_out + 1
         text = self._randomText()
         line = self.data[text]
         lenght = len(self.data[text])
-        st = random.randint(0, lenght - l - 1)
-        input_line = self._textToNum(line[st:st+l])
-        output_line = input_line[1:]
-        output_line.append(self._textToNum(line[st+l]))
+        st = random.randint(0, lenght - l_in - 1 - offset)
+        input_line = self.textToNum(line[st:st+l_in])
+        output_line = self.textToNum(line[st+offset:st+offset+l_out])
         return input_line, output_line
+    def makeInandOutTensor(self,l_in, l_out, batch_size):
+        in_tensor = np.zeros((l_in, batch_size, self.nchar))
+        out_tensor = np.zeros((l_out, batch_size, self.nchar))
+        for i in range(batch_size):
+            inp, outp = self.randomInandOut(l_in,l_out,batch_num=batch_size)
+            in_tensor[np.arange(0,l_in),i,inp] = 1
+            out_tensor[np.arange(0,l_out),i,outp] = 1
+        return in_tensor, out_tensor
+
+    def tensorToText(self,tensor):
+        '''
+        Input: (T,1,nchar)
+        '''
+        T = tensor.shape[0]
+        inp = tensor.reshape((T,self.nchar))
+        onehot = np.argmax(inp,axis=1)
+        print(onehot)
+        return self.numToText(onehot)
+    def penalty(self):
+        collect = np.zeros(self.nchar)
+        for text in self.data:
+            for char in text:
+                collect[self.allchar.find(char)] += 1
+        return collect/np.sum(collect) * self.nchar
+    def textTotensor(self, text):
+        numlist = self.textToNum(text)
+        n = len(text)
+        out = np.zeros((n,1,self.nchar))
+        out[np.arange(0,n),0,numlist] = 1
+        return out
--- helper/model.py
+++ helper/model.py
@@ -1,7 +1,67 @@
 import tensorflow as tf
 
 class RNN(object):
-    """docstring for RNN."""
-    def __init__(self, num_layers = 2):
+    """docstring for RNN.
+    input: (T,N,L)
+    T - time-step
+    N - batch-size
+    L - lenght(n_char)
+    """
+    def __init__(self,hidden_size,hid_layers = 2):
         super(RNN, self).__init__()
-        lstm = tf.contrib.rnn.BasicLSTMCell(lstm_size)
+        def lstm_cell_mid():
+            return tf.contrib.rnn.BasicLSTMCell(512)
+        def lstm_cell():
+            return tf.contrib.rnn.BasicLSTMCell(hidden_size)
+        if hid_layers > 1:
+            self.stacked_lstm = tf.contrib.rnn.MultiRNNCell([lstm_cell_mid() for _ in range(hid_layers)] + [lstm_cell()])
+
+    def batchnorm(self, x, training=True):
+        with tf.variable_scope("batch"):
+            return tf.layers.batch_normalization(x, training=training)
+    def forward(self,input_val, output_val, out_l, step, batch_size, training):
+        state = self.stacked_lstm.zero_state(batch_size, tf.float32)
+        out = tf.reshape(input_val[0],[1,batch_size,-1])
+        loss = tf.Variable(0., tf.float32)
+        for i in range(step):
+            tmp, state = self.stacked_lstm(input_val[i], state)
+            if i > 0:
+                with tf.variable_scope("norm") as scope:
+                    scope.reuse_variables()
+                    tmp = self.batchnorm(tmp, training=training)
+            else:
+                with tf.variable_scope("norm") as scope:
+                    tmp = self.batchnorm(tmp, training=training)
+            out = tf.concat([out, tf.reshape(tmp,[1,batch_size,-1])],0)
+            if i >= step - out_l:
+                loss += tf.reduce_mean(tf.losses.softmax_cross_entropy(output_val[i-(step-out_l)], tmp))
+        return tf.slice(out,[step-out_l,0,0],[out_l,-1,-1]), loss/step
+    def tensorOnehot(self, x):
+        '''
+        x:(1,nchar)
+        '''
+        pos = tf.argmax(x,axis=1)
+        return tf.sparse_tensor_to_dense(tf.SparseTensor([[0,pos[0]]],[1.],tf.cast(tf.shape(x),dtype=tf.int64)))
+
+    def test_forward(self, input_val, out_l, T, nchar):
+        '''
+        input_val: (T,1,nchar)
+        '''
+        state = self.stacked_lstm.zero_state(1, tf.float32)
+        out = tf.reshape(input_val[0],[1,1,nchar])
+        tmp = None
+        for i in range(T):
+            tmp, state = self.stacked_lstm(input_val[i], state)
+        inp = self.tensorOnehot(tmp)
+        for i in range(out_l):
+            tmp, state = self.stacked_lstm(inp, state)
+            try:
+                with tf.variable_scope("norm") as scope:
+                    scope.reuse_variables()
+                    tmp = self.batchnorm(tmp, training=False)
+            except:
+                with tf.variable_scope("norm") as scope:
+                    tmp = self.batchnorm(tmp, training=False)
+            out = tf.concat([out, tf.reshape(tmp,[1,1,-1])],0)
+            inp = self.tensorOnehot(tmp)
+        return out
